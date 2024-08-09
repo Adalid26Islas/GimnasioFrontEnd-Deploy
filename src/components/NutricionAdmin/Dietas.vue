8059c9c8f6c3fa8a4fae6ff16335be6e39c03b4b
Adalid26Islas <adalidislasquintero@gmail.com>
2024-08-08 19:57:30 -0500

    Update Dietas Form

--- src/components/NutricionAdmin/Dietas.vue
+++ src/components/NutricionAdmin/Dietas.vue
@@ -7,18 +7,18 @@
                     <p class="text-gray-100 dark:text-gray-100 mb-6">Ingresa la información Correspondiente para crear una nueva dieta.</p>
                     <form>
                         <div class="grid grid-cols-1 md:grid-cols-3 gap-4 mb-6">
-                            <input type="text" placeholder="Nombre de usuario" class="border p-2 rounded w-full font-medium bg-gray-100 border border-gray-200 placeholder-gray-500 text-sm focus:outline-none focus:border-gray-400 focus:bg-white">
-                            <input type="text" placeholder="Correo electronico del usuario" class="border p-2 rounded w-full font-medium bg-gray-100 border border-gray-200 placeholder-gray-500 text-sm focus:outline-none focus:border-gray-400 focus:bg-white">
-                            <select class="border  rounded w-full font-medium bg-gray-100 border border-gray-200 placeholder-gray-500 text-sm focus:outline-none focus:border-gray-400 focus:bg-white">
+                            <input type="text" placeholder="Nombre de usuario" class=" p-2 rounded-lg w-full font-medium bg-gray-100 border border-gray-200 placeholder-gray-500 text-sm focus:outline-none focus:border-gray-400 focus:bg-white">
+                            <input type="text" placeholder="Correo electronico del usuario" class=" p-2 rounded-lg w-full font-medium bg-gray-100 border border-gray-200 placeholder-gray-500 text-sm focus:outline-none focus:border-gray-400 focus:bg-white">
+                            <select class="rounded-lg w-full font-medium bg-gray-100  border-gray-200 placeholder-gray-500 text-sm focus:outline-none focus:border-gray-400 focus:bg-white">
                                 <option value="">Sexo</option>
                                 <option value="masculino">Masculino</option>
                                 <option value="femenino">Femenino</option>
                             </select>
                         </div>
                         <div class="grid grid-cols-1 md:grid-cols-3 gap-4 mb-6">
-                            <input type="number" placeholder="Altura en Centimetros" class="border p-2 rounded w-full font-medium bg-gray-100 border border-gray-200 placeholder-gray-500 text-sm focus:outline-none focus:border-gray-400 focus:bg-white">
-                            <input type="number" placeholder="Peso actual en Kilogramos" class="border p-2 rounded w-full font-medium bg-gray-100 border border-gray-200 placeholder-gray-500 text-sm focus:outline-none focus:border-gray-400 focus:bg-white">
-                            <select class="border  rounded w-full font-medium bg-gray-100 border border-gray-200 placeholder-gray-500 text-sm focus:outline-none focus:border-gray-400 focus:bg-white">
+                            <input type="number" placeholder="Altura en Centimetros" class=" p-2 rounded-lg w-full font-medium bg-gray-100 border border-gray-200 placeholder-gray-500 text-sm focus:outline-none focus:border-gray-400 focus:bg-white">
+                            <input type="number" placeholder="Peso actual en Kilogramos" class="border p-2 rounded-lg w-full font-medium bg-gray-100 border border-gray-200 placeholder-gray-500 text-sm focus:outline-none focus:border-gray-400 focus:bg-white">
+                            <select class=" rounded-lg w-full font-medium bg-gray-100  border-gray-200 placeholder-gray-500 text-sm focus:outline-none focus:border-gray-400 focus:bg-white">
                                 <option value="">Objetivo de la dieta</option>
                                 <option value="perdida">Perdida de peso</option>
                                 <option value="aumento">Aumento de masa muscular</option>
@@ -26,13 +26,13 @@
                             </select>
                         </div>
                         <div class="grid grid-cols-1 md:grid-cols-3 gap-4 mb-6">
-                            <select class="border  rounded w-full font-medium bg-gray-100 border border-gray-200 placeholder-gray-500 text-sm focus:outline-none focus:border-gray-400 focus:bg-white">
+                            <select class="rounded-lg w-full font-medium bg-gray-100 border border-gray-200 placeholder-gray-500 text-sm focus:outline-none focus:border-gray-400 focus:bg-white">
                                 <option value="">Tipo de Ejercicios Recomendados</option>
                                 <option value="cardio">Cardio</option>
                                 <option value="pesas">Levantamiento de pesas</option>
                                 <option value="tecnicos">Ejercicios tecnicos</option>
                             </select>
-                            <select class="border  rounded w-full font-medium bg-gray-100 border border-gray-200 placeholder-gray-500 text-sm focus:outline-none focus:border-gray-400 focus:bg-white">
+                            <select class="border  rounded-lg w-full font-medium bg-gray-100 border border-gray-200 placeholder-gray-500 text-sm focus:outline-none focus:border-gray-400 focus:bg-white">
                                 <option value="">Dias de Ejercicio</option>
                                 <option value="cardio">1 dia a la semana</option>
                                 <option value="cardio">2 dias a la semana</option>
@@ -40,7 +40,7 @@
                                 <option value="cardio">4 dias a la semana</option>
                                 <option value="cardio">5 dias a la semana</option>
                             </select>
-                            <input type="number" placeholder="Calorias diarias" class="border p-2 rounded w-full font-medium bg-gray-100 border border-gray-200 placeholder-gray-500 text-sm focus:outline-none focus:border-gray-400 focus:bg-white">
+                            <input type="number" placeholder="Calorias diarias" class=" p-2 rounded-lg w-full font-medium bg-gray-100  border-gray-200 placeholder-gray-500 text-sm focus:outline-none focus:border-gray-400 focus:bg-white">
                         </div>
                         <textarea name="" id="" cols="30" class="w-full mb-6 rounded-lg font-medium bg-gray-100 border border-gray-200 placeholder-gray-500 text-sm focus:outline-none focus:border-gray-400 focus:bg-white" placeholder="Alimentos recomendados, restricciones u observaciones"></textarea>
                         <button type="button" id="dieta" class="px-4 py-2 w-[20%] rounded bg-red-600 text-white hover:bg-gray-600 focus:outline-none transition-colors">
@@ -53,7 +53,7 @@
         </section>
         <section class="m-4">
             <table class="w-full bg-white text-left text-sm text-gray-900 rounded ">
-                <thead class="bg-gray-50 rounded text-center">
+                <thead class="bg-gray-50 text-center">
                     <tr>
                         <th scope="col" class="px-6 py-4 bg-gray-900 font-medium text-gray-100 rounded-l-md">Nombre de usuario</th>
                         <th scope="col" class="px-6 py-4 bg-gray-900 font-medium text-gray-100">Correo electronico</th>
@@ -63,7 +63,7 @@
                     </tr>
                 </thead>
                 <tbody class="bg-gray-200">
-                    <tr class="hover:bg-gray-500">
+                    <tr class="hover:bg-gray-500 ">
                         <th class="h-[50px] text-center">
                             <span>adalid_26</span>
                         </th>
@@ -143,7 +143,6 @@
                         </th>
                     </tr>
 
-                    <!-- 
                     <tr class="hover:bg-gray-500">
                         <th class="h-[50px] text-center">
                             <span>adalid_26</span>
@@ -183,6 +182,8 @@
                             </button>
                         </th>
                     </tr>
+
+                <!-- 
 
                     <tr class="hover:bg-gray-500">
                         <th class="h-[50px] text-center">
