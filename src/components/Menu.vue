bb11ab17cd99feddf922781689526888cc4a3716
Adalid26Islas <adalidislasquintero@gmail.com>
2024-08-23 09:40:45 -0600

    Eliminaciòn de comentario para la ruta de instructores en el menu

--- src/components/Menu.vue
+++ src/components/Menu.vue
@@ -194,11 +194,11 @@
                        <a href="#" class="flex items-center w-full p-2 text-gray-900 transition duration-75 rounded-lg pl-11 group hover:bg-gray-100 dark:text-white dark:hover:bg-gray-700">Empleados</a>
                      </RouterLink>
                     </li>
-                    <!--<li>
+                    <li>
                      <RouterLink to="/instructores">
                        <a href="#" class="flex items-center w-full p-2 text-gray-900 transition duration-75 rounded-lg pl-11 group hover:bg-gray-100 dark:text-white dark:hover:bg-gray-700">Instructores</a>
                      </RouterLink>
-                    </li>-->
+                    </li>
                     <li>
                      <RouterLink to="/horarios">
                        <a href="#" class="flex items-center w-full p-2 text-gray-900 transition duration-75 rounded-lg pl-11 group hover:bg-gray-100 dark:text-white dark:hover:bg-gray-700">Horarios</a>
