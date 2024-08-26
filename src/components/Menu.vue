43c78d5f2098a7e9e80d217b9d1f0deae0836f1d
Jesus Flores Valencia <valenciacerogpt@gmail.com>
2024-08-26 00:15:28 -0600

    Se corrigio la ruta de prestamos a adeudos

--- src/components/Menu.vue
+++ src/components/Menu.vue
@@ -238,7 +238,7 @@
                      </RouterLink>
                     </li>
                     <li>
-                     <RouterLink to="/prestamos">
+                     <RouterLink to="/adeudos">
                        <a href="#" class="flex items-center w-full p-2 text-gray-900 transition duration-75 rounded-lg pl-11 group hover:bg-gray-100 dark:text-white dark:hover:bg-gray-700">Prestamo</a>
                      </RouterLink>
                     </li>
